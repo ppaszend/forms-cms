e2206b52b8f79430c6f69d79a581e563b98af572
Paweł Paszenda <ppaszend@gmail.com>
2023-01-25 10:41:35 +0100

    1

---
+++ src/components/modals/addNewFieldModal.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "addNewFieldModal"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/FormsListView.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "FormsListView"
+}
+</script>
+
+<style scoped>
+
+</style>
